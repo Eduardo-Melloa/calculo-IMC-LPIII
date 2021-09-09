765e336c6e8847e5aacce62a6625f39ebc74f799
Eduardo Alves <eduardomma99@gmail.com>
2021-09-09 12:17:09 -0300

    para revisao

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,67 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header>
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Cálculo IMC!</ion-title>
       </ion-toolbar>
     </ion-header>
     
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+    <ion-content>
+ 
+        <ion-grid>
+          <!--Inserção de nome  -->
+          <ion-row>
+            <ion-col>
+              <ion-label>Nome:</ion-label>
+              <ion-input type="text" id="nome" placeholder="Digite o seu nome" v-model="nome"></ion-input>
+            </ion-col>
+          </ion-row>
+
+          <!--Inserção de altura -->
+          <ion-row>
+            <ion-col>
+              <ion-label>Altura:</ion-label>
+              <ion-input inputmode="numeric" type="number" id="altura" v-model.number="altura" clear-on-edit="true"></ion-input>
+            </ion-col>
+          </ion-row>
+          
+          <!--Inserção de peso -->
+          <ion-row>
+            <ion-col>
+              <ion-label>Peso:</ion-label>
+              <ion-input inputmode="numeric" type="number" id="peso" v-model.number="peso" clear-on-edit="true"></ion-input>
+            </ion-col>
+          </ion-row>
+
+          <!--Botão para realização do cálculo IMC-->
+          <ion-row>
+            <ion-col>
+              <ion-button @click="calcularIMC()" expand="block">Calcule IMC!</ion-button>
+            </ion-col>
+          </ion-row>
+
+          <!-- Coluna para mostra de resultados-->
+          <ion-row >
+            <ion-col>
+              <ion-textarea v-if="calculado" type="text" id="mensagem" v-model="mensagem"></ion-textarea>
+            </ion-col>            
+          </ion-row>
+
+          <!--Botão para atualização de página-->
+          <ion-row>
+            <ion-col>
+              <ion-button v-if="calculado" @click="atualizarPagina()">Novo cálculo</ion-button>
+            </ion-col>
+          </ion-row>
+            
+        </ion-grid>
+
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonLabel, IonInput, IonButton, IonTextarea } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -32,37 +71,68 @@ export default defineComponent({
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
+    IonToolbar,
+    IonGrid, 
+    IonRow, 
+    IonCol, 
+    IonLabel, 
+    IonInput, 
+    IonButton, 
+    IonTextarea,
+  },
+  data(){
+    return{
+      nome:'',
+      altura:0,
+      peso:0,
+      mensagem:'',
+      calculado:false, //variavel para definir se o calculo já foi feito para mostra de resultados
+    }
+  },
+  methods:{
+    //Cálculo do IMC e criação da mensagem de retorno se baseando em tabela do site programasaudefacil
+    async calcularIMC(){
+      const IMC=this.peso/(this.altura*this.altura);
+      if (IMC<18.5){
+        this.mensagem = 'Seu IMC é: '+IMC+'\nVoce está classificado com Magreza'
+      }else if (IMC<24.9){
+        this.mensagem = 'Seu IMC é: '+IMC+'\nVoce está classificado com IMC Normal'        
+      }else if (IMC<29.9){
+        this.mensagem = 'Seu IMC é: '+IMC+'\nVoce está classificado com Sobrepeso'        
+      }else if (IMC<39.9){
+        this.mensagem = 'Seu IMC é: '+IMC+'\nVoce está classificado com Obesidade'        
+      }else {
+        this.mensagem = 'Seu IMC é: '+IMC+'\nVoce está classificado com Obesidade II'        
+      }
+      this.calculado=true;
+    },
+    atualizarPagina(){
+      window.location.reload()
+    },
+  },
+  
 });
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+ion-input{
+  border:solid 2px black;
+  background: black;
+  border-radius: 10px;
+  --placeholder-color:white;
+  color:white;
+  font-size: 15px;
 }
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+ion-title{
+  font-size: 24px;
+  font-weight: 700;
 }
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
+ion-label{
+  font-size: 18px;
+  font-weight: 500;
 }
-
-#container a {
-  text-decoration: none;
+ion-textarea{
+  border:solid 2px black;
+  border-radius: 10px;
 }
 </style>
